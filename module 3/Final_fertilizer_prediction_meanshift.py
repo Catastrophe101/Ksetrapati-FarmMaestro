ef3d79080130b4f4aa31750e1f59bb4415105abe
Kyouma <2015mitesh.methani@ves.ac.in>
2019-03-27 13:36:19 +0530

    Add files via upload

--- module 3/Final_fertilizer_prediction_meanshift.py
+++ module 3/Final_fertilizer_prediction_meanshift.py
@@ -72,18 +72,18 @@ plt.show()
 # In[21]:
 
 
-# In[4]:
+# In[6]:
 
 
-arr=[0,120,120]
+arr=[80,25,25] #total_fertilizer_req
 b=arr[0]+arr[1]+arr[2]
 farr=[0,0,0]
 for i in range(0,3):
     farr[i]=(arr[i]*100)/b
-print(farr)
+print(farr) #amt of fertilizer in 100kg
 
 
-# In[5]:
+# In[7]:
 
 
 val=np.asarray(farr)
@@ -92,7 +92,7 @@ prediction=meanShift.predict(values)
 print("Predicted Cluster "+str(prediction))
 
 
-# In[6]:
+# In[8]:
 
 
 output=df_tr[['Fert_name','n%','p%','k%','n_ratio','p_ratio','k_ratio','price/kg','price(100kg)bag']].loc[df_tr['clusters'] ==  prediction[0]]
@@ -101,7 +101,7 @@ df_test=df_tr[tr_clmns]
 output
 
 
-# In[7]:
+# In[17]:
 
 
 a=[0,0,0]
@@ -111,21 +111,36 @@ for row in output.itertuples():
     for i in range(0,3):
         if(row[i+2]!=0):
             a[i]=arr[i]/row[i+2]
-    a.sort()
-    if(a[0]==0):
+    a1=[0,0,0]
+    for i in range(0,3):
+        a1[i]=a[i]
+    a1.sort()
+    if(a1[0]==0 and a1[1]==0):
+        for i in range(2,3):
+            a1[i]=a1[2]
+            cp_a[i]=a1[i]*row[9]
+    elif(a1[0]==0):
         for i in range(1,3):
-            a[i]=a[1]
-            cp_a[i]=a[i]*row[9]
+            a1[i]=a1[1]
+            cp_a[i]=a1[i]*row[9]
     else:
         for i in range(0,3):
-            a[i]=a[0]
-            cp_a[i]=a[i]*row[9]
+            a1[i]=a1[0]
+            cp_a[i]=a1[i]*row[9]
+    x=0
+    for i in range(0,3):
+        if(a1[i]!=0):
+            x=a1[i]
+            break
+    for i in range(0,3):
+        if(a[i]!=0):
+            a[i]=x
     for i in range(0,3):
         b[i]=a[i]*row[i+2]
             
 
 
-# In[8]:
+# In[18]:
 
 
 print(a)
@@ -133,7 +148,7 @@ print(cp_a)
 print(b) #the total kg of fertilizer the predicted cluster can provide of n-p-k proportion
 
 
-# In[9]:
+# In[19]:
 
 
 # triple superphosphate 0-46-0 837.2
@@ -155,7 +170,7 @@ c[2]=c[2]/60
 cp_c[2]=c[2]*1570
 
 
-# In[10]:
+# In[20]:
 
 
 print(c) # amount of fertilizer required
@@ -163,7 +178,7 @@ print(cp_c) # rate of fertilizer
 # additional fertilizers urea --- triple superphosphate --- potassium chloride
 
 
-# In[11]:
+# In[21]:
 
 
 tot_cp=[0,0,0]
@@ -176,14 +191,14 @@ for i in range(0,3):
 tot=tot+cp
 
 
-# In[12]:
+# In[22]:
 
 
 print(tot_cp) # total cost price of fertlizer
 print(tot)#total
 
 
-# In[13]:
+# In[23]:
 
 
 new = output[['Fert_name', 'price/kg']].copy()
@@ -191,19 +206,19 @@ new = output[['Fert_name', 'price/kg']].copy()
 new1=list(new.values.flatten())
 
 
-# In[14]:
+# In[24]:
 
 
 #print(new1[1])
 
 
-# In[15]:
+# In[25]:
 
 
 fin={'Suggested_fert':[new1[0]],'Suggested_fert_price/kg':[new1[1]],'Reqd_N':[b[0]],'Reqd_P':[b[1]],'Reqd_K':[b[2]],'Total_price':[cp_a[2]],'Urea_reqd':[c[0]],'triple superphosphate_reqd':[c[1]],'Potassium_chloride_reqd':[c[2]],'Urea_price':[cp_c[0]],'triple superphosphate_price':[cp_c[1]],'Potassium_chloride_price':[cp_c[2]],'Grand_total':tot}
 
 
-# In[16]:
+# In[26]:
 
 
 fin1=pd.DataFrame(fin,columns=['Suggested_fert','Suggested_fert_price/kg','Reqd_N','Reqd_P','Reqd_K','Total_price','Urea_reqd','triple superphosphate_reqd','Potassium_chloride_reqd','Urea_price','triple superphosphate_price','Potassium_chloride_price','Grand_total'])
